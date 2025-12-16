89248bcb7765c98b94e3cb176bc33f891c970cb3
Daniyar <damai3350@gmail.com>
2025-12-16 22:09:57 +0300

    add user repository interface

---
+++ internal/repository/user_repo.go
@@ -0,0 +1,26 @@
+package repository
+
+// Handler не должен знать,
+// Service не должен знать,
+// ГДЕ и КАК хранятся пользователи.
+import (
+	"github.com/daniyar23/crm/internal/domain"
+)
+
+type UserRepository interface { // Мы объявляем контракт, а не реализацию
+	GetUserByID(id uint) (domain.User, error)
+	// Очевидно и просто — одна ответственность.
+	GetAllUsers() ([]domain.User, error)
+	// Для начала CRM этого достаточно.
+	CreateUser(user domain.User) (domain.User, error)
+	// принимает чистую domain-модель
+	// возвращает:
+	// 		пользователя (уже с ID)
+	// 		ошибку (если БД упала, конфликт и т.п.)
+	// Почему возвращаем User, а не ID?
+	// Потому что:
+	// 		сервису часто нужен объект целиком
+	// 		это гибче
+	UpdateUser(user domain.User) error
+	DeleteUser(id uint) error
+}
