27609d55e9ee41a8e0ac33d8d0e74d729493beb7
Daniyar <damai3350@gmail.com>
2025-12-31 23:08:05 +0300

    реализован ui

--- cmd/crm/main.go
+++ cmd/crm/main.go
@@ -25,14 +25,29 @@ func main() {
 	}
 	defer dbConn.Close()
 
+	// User
 	userRepo := db.NewUserPostgresRepository(dbConn)
 	userService := services.NewUserService(userRepo)
 	userHandler := delivery.NewUserHandler(userService)
-
 	api := router.Group("/api")
 	userHandler.RegisterRoutes(api)
 
+	// Company
+	companyRepo := db.NewCompanyPostgresRepository(dbConn)
+	companyService := services.NewCompanyService(companyRepo)
+	companyHandler := delivery.NewCompanyHandler(companyService)
+	companyHandler.RegisterRoutes(api)
+
+	// HTML страницы
+	router.LoadHTMLFiles("static/index.html")
+	router.Static("/static", "./static")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.html", nil)
+	})
+
+	// Запуск сервера
 	if err := router.Run(cfg.HTTPServer.Address); err != nil {
 		log.Fatal(err)
 	}
+
 }
